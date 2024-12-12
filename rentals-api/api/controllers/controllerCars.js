f7c431a92154d5c5bc98d8bcc0e64c6994c5ed0b
AndMJ <andrejoao070@gmail.com>
2024-12-12 17:55:41 +0000

    UPDATE controller.newCar function

--- rentals-api/api/controllers/controllerCars.js
+++ rentals-api/api/controllers/controllerCars.js
@@ -1,6 +1,9 @@
+const {companyJoiSchema, carJoiSchema} = require("../db/dataValidation")
+const Joi = require("joi");
+
 module.exports = () => {
     const modelCars = require("../models/modelCars")()
-    //const modelCompanies = require("../models/modelCompanies")()
+    const modelCompanies = require("../models/modelCompanies")()
 
     const controller = []
     controller.getCarsList = async (req, res) => {
@@ -14,14 +17,30 @@ module.exports = () => {
 
     controller.newCar = async (req, res) => {
         try {
-            const company = await modelCompanies.getByID("378n4tvoq")
+            //DATA VALIDATION
+            let companyIsValid = companyJoiSchema.validate({
+                _id: req.body.manufacturer
+            })
+            if (companyIsValid.error){
+                return res.status(404).json(companyIsValid.error.details[0].message)
+            }
 
-            // const result = await modelCars.create({
-            //     manufacturer: "BMW",
-            //     model: "E30",
-            //     year: 1998
-            // })
-            // res.status(200).json(result)
+            let carIsValid = carJoiSchema.validate({
+                model: req.body.model.trim(),
+                manufacturer: req.body.manufacturer,
+                year: req.body.year
+            })
+            if (carIsValid.error){
+                return res.status(404).json(carIsValid.error.details[0].message)
+            }
+
+            const result = await modelCars.create({ //TODO: incorrect approach for registering cars, it is not doing .populate() its not embedding? idk
+                model: req.body.model.trim(),
+                manufacturer: req.body.manufacturer,
+                year: req.body.year
+            })
+
+            res.status(200).json(result)
         } catch (e) {
             return res.status(404).json(e.message)
         }
